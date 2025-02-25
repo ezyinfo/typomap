0af2f46d5f6e9db0bf6c3c83d7f7a165835e8b7d
Cedric Dufouil <cedric@ezyinfo.fr>
2025-02-25 12:05:32 +0100

    Separate front from back

---
+++ backapp/api/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello world</title>
+</head>
+
+<body>
+    <?php
+
+    // Connect to database
+
+
+    $host = "mysql";
+    $base = "typomapdb";
+    $user = "typo";
+    $pass = "Pass--W0rd";
+
+
+    // Connect to the base
+    try {
+        $conn = new PDO("mysql:host=$host;dbname=$base", $user, $pass);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
+
+    ?>
+</body>
+
+</html>
